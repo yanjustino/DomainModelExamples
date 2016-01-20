bf406dd47692ab17f266171b742ef364fe8be5f6
yan de lima justino <yan_limaj@hotmail.com>
2016-01-20 08:50:29 -0300

    implmentação dos créditos e testes

---
+++ CredPlus/CredPlus/Financiamento/CredPlus.Financiamento.Tests/Domain.Model/Creditos/CreditoTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CredPlus.Financiamento.Domain.Model.Creditos;
+
+namespace CredPlus.Financiamento.Tests.Domain.Model.Creditos
+{
+    [TestClass]
+    public class CreditoTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TesteValorMaximoCredito()
+        {
+            var credito = new Credito(21000);
+        }
+    }
+}
---
+++ CredPlus/CredPlus/Financiamento/CredPlus.Financiamento/Domain.Model/Creditos/Credito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CredPlus.Financiamento.Domain.Model.Creditos
+{
+    public class Credito
+    {
+        public Credito(decimal valor)
+        {
+            if (valor > 20000)
+                throw new Exception("Valor inválido");
+        }
+
+    }
+}
